1fba7807185b2209b1b108e4776284cdee877dc4
alexisvannay <vannayalexis@gmail.com>
2025-04-16 18:54:34 +0200

    Update script-admin.js

--- script-admin.js
+++ script-admin.js
@@ -110,7 +110,7 @@ function ajouterLigne(j = "", h = "") {
   div.className = "horaire-ligne";
   div.innerHTML = `<input class="jours" placeholder="Jour ou plage" value="${j}">
                    <input class="heures" placeholder="Horaires" value="${h}">
-                   <button>🗑</button>`;
+                   <button class="bouton-supprimer-ligne">🗑</button>`;
   div.querySelector("button").addEventListener("click", () => div.remove());
   container?.appendChild(div);
 }
